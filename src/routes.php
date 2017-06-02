efc17d630b476d33e9ed2f88648a58152dac8a73
Miguel Pazo <miguel.ps19@gmail.com>
2017-06-01 23:48:56 -0500

    test ready

--- src/routes.php
+++ src/routes.php
@@ -11,8 +11,22 @@
 
 $app->get('/', function ($request, $response, $args) {
     $lstEmployees = json_decode(file_get_contents('../database/employees.json'));
+    $lstEmployeesFinal = [];
 
-    $args['lstEmployees'] = $lstEmployees;
+    $email = $request->getParam('email', '');
+
+    if (!$email) {
+        $lstEmployeesFinal = $lstEmployees;
+    } else {
+        foreach ($lstEmployees as $key => $employee) {
+            if (strpos($employee->email, $email) !== false) {
+                $lstEmployeesFinal[] = $employee;
+            }
+        }
+    }
+
+    $args['lstEmployees'] = $lstEmployeesFinal;
+    $args['email'] = $email;
 
     return $this->renderer->render($response, 'index.phtml', $args);
 });
@@ -21,6 +35,50 @@ $app->get('/{id}', function ($request, $response, $args) {
     $lstEmployees = json_decode(file_get_contents('../database/employees.json'));
 
     $id = $request->getAttribute('id');
+    $employee = null;
+    $strSkills = "";
 
-    return $this->renderer->render($response, 'index.phtml', $args);
+    foreach ($lstEmployees as $key => $value) {
+        if ($value->id == $id) {
+            $employee = $value;
+            break;
+        }
+    }
+
+    if ($employee) {
+        $skills = $employee->skills;
+
+        foreach ($skills as $key => $skill) {
+            $strSkills .= ",{$skill->skill}";
+        }
+    }
+
+    $args['employee'] = $employee;
+    $args['skills'] = substr($strSkills, 1);
+
+    return $this->renderer->render($response, 'detail.phtml', $args);
+});
+
+$app->get('/{min}/{max}', function ($request, $response, $args) {
+    $lstEmployees = json_decode(file_get_contents('../database/employees.json'));
+    $min = $request->getAttribute('min', 0);
+    $max = $request->getAttribute('max', 100000);
+
+    $lstEmployeesFinal = [];
+
+    foreach ($lstEmployees as $key => $employee) {
+        $salary = (double)str_replace(['$', ','], ['', ''], $employee->salary);
+
+        if ($salary > $min && $salary < $max) {
+            $lstEmployeesFinal[] = $employee;
+        }
+    }
+
+    $args['lstEmployees'] = $lstEmployeesFinal;
+    $xml = $this->renderer->render($response, 'employees.phtml', $args);
+
+    return $response->withStatus(200)
+        ->withHeader('Content-Type', 'text/xml')
+        ->write('', $xml);
+
 });
